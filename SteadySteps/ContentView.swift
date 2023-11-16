5f4e08146fa14a7e7042a77828e2e6b978ad4e7a
Rob <robert.homa2006@gmail.com>
2023-11-16 12:35:36 -0500

    Initial Commit

---
+++ SteadySteps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SteadySteps
+//
+//  Created by Robert Homa on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SteadySteps/SteadyStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteadyStepsApp.swift
+//  SteadySteps
+//
+//  Created by Robert Homa on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct SteadyStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
